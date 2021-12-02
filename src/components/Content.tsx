966a66fcc7ce10c5453233d882f0a6258905f472
voigtito <voigt.gustavoh@gmail.com>
2021-12-02 08:56:44 -0300

    Separating components from App.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,16 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+interface Props {
+  movies: MovieProps[];
+}
+
+export function Content({movies}: Props) {
+  return (    
+    <div className="movies-list">
+    {movies.map(movie => (
+      <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+    ))}
+  </div>
+  )
 }
